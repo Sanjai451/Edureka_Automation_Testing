d9630e03e742a08318149444011f1532425f91aa
Sanjai451 <sanjaikumarr451@gmail.com>
2026-04-17 18:10:55 +0530

    added step definitions

--- src/test/java/com/edureka/stepDefinition/CommunityQuestionTests.java
+++ src/test/java/com/edureka/stepDefinition/CommunityQuestionTests.java
@@ -1,13 +1,16 @@
 package com.edureka.stepDefinition;
 
+import com.edureka.utility.AllFunctionality;
 import com.edureka.utility.Base;
 import com.edureka.utility.Pages;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class CommunityQuestionTests {
+public class CommunityQuestionTests extends AllFunctionality {
+	
 	private Base base;
+	
 	public CommunityQuestionTests(Base base) {
 		this.base = base;
 	}
@@ -18,18 +21,18 @@ public class CommunityQuestionTests {
 	}
 
 	@When("user clicks on question {string}")
-	public void user_clicks_on_question(String string) {
-		
+	public void user_clicks_on_question(String question) {
+		Pages.communityHomePage.clickOnQuestion(base.driver, question);
 	}
 
 	@When("user should see the question fully")
 	public void user_should_see_the_question_fully() {
-
+		
 	}
 
 	@When("user add comment for the question")
 	public void user_add_comment_for_the_question() {
-
+		Pages.communityQuestionPage.addCommentForQuestion(base.driver, "Test comment");
 	}
 
 	@Then("verify the comment is added")
@@ -39,7 +42,7 @@ public class CommunityQuestionTests {
 
 	@When("user clicks on Ask a Question")
 	public void user_clicks_on_ask_a_question() {
-
+		Pages.communityHomePage.clickAskQuestion();
 	}
 
 	@When("user enters question details")
@@ -50,25 +53,28 @@ public class CommunityQuestionTests {
 		// Map<K, List<V>>. E,K,V must be a String, Integer, Float,
 		// Double, Byte, Short, Long, BigInteger or BigDecimal.
 		
+		// here it will fill and submit 
+		Pages.communityAskQuestion.fillQuestionDetails(null, null, null);
 	}
 
 	@When("user submits the question")
 	public void user_submits_the_question() {
-
+		// will be submitted prior
 	}
 
 	@Then("question should be posted successfully in the forum")
 	public void question_should_be_posted_successfully_in_the_forum() {
-
+		String titleString = Pages.communityQuestionAddedPage.getTitleOfQuestion();
+		System.out.println("Question Added : " + titleString);
 	}
 	
 	@When("user add answer for the question")
 	public void user_add_answer_for_the_question() {
-
+		Pages.communityQuestionPage.addAnswerForQuestion(base.driver, "Test Answer");
 	}
 
 	@Then("verify the answer is added")
 	public void verify_the_answer_is_added() {
-
+		
 	}
 }
